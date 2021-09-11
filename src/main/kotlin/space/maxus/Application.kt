920a773800e42b404b6898d1ae9171076fe916df
Maxuss <maxus.info.mail@gmail.com>
2021-09-11 17:31:04 +0300

    Async!

--- src/main/kotlin/space/maxus/Application.kt
+++ src/main/kotlin/space/maxus/Application.kt
@@ -1,12 +1,14 @@
 package space.maxus
 
-import io.ktor.server.netty.*
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.server.netty.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import space.maxus.api.*
 import space.maxus.json.JsonConverter
 import space.maxus.util.Pages
@@ -15,7 +17,6 @@ import java.util.*
 
 fun main(args: Array<String>) {
     val example = Key.generate(UUID.randomUUID())
-
     println("Example key: ${example.value}")
     EngineMain.main(args)
 }
@@ -34,58 +35,85 @@ fun Application.module() {
     install(StatusPages) {
         statusFile(HttpStatusCode.NotFound, HttpStatusCode.InternalServerError, filePattern = "error#.html")
         exception<NotFoundException> {
-            call.respondText(
-                JsonConverter.endpointJson(ErrorEndpoint(404, "NOT_FOUND", "Endpoint not found: ${call.request.uri}")),
-                ContentType.Application.Json
-            )
+            withContext(Dispatchers.Default) {
+                call.respondText(
+                    endpoint(
+                        ErrorEndpoint(
+                            404,
+                            "NOT_FOUND",
+                            "Endpoint not found: ${call.request.uri}"
+                        )
+                    ),
+                    ContentType.Application.Json
+                )
+            }
         }
         exception<Throwable> {
-            call.respondText(
-                JsonConverter.endpointJson(ErrorEndpoint(500, "INTERNAL_SERVER_ERROR", "Unknown internal error occurred")),
-                ContentType.Application.Json
-            )
+            withContext(Dispatchers.Default) {
+                call.respondText(
+                    endpoint(
+                        ErrorEndpoint(
+                            500,
+                            "INTERNAL_SERVER_ERROR",
+                            "Unknown internal error occurred"
+                        )
+                    ),
+                    ContentType.Application.Json
+                )
+            }
         }
     }
 
     routing {
         get("/api") {
-            call.respondText(
-                JsonConverter.endpointJson(HeaderEndpoint(200, "SkyblockSketchpad API v1")),
-                contentType = ContentType.Application.Json
-            )
+            withContext(Dispatchers.Default) {
+                call.respondText(
+                    endpoint(HeaderEndpoint(200, "SkyblockSketchpad API v1")),
+                    contentType = ContentType.Application.Json
+                )
+            }
         }
     }
 
     routing {
         get("/") {
-            call.respondText(
-                Pages.aboutPage,
-                contentType = ContentType.Text.Html
-            )
+            withContext(Dispatchers.Default) {
+                call.respondText(
+                    Pages.aboutPage,
+                    contentType = ContentType.Text.Html
+                )
+            }
         }
     }
 
     routing {
         get("/api/users") {
-            val ep = Security.validateKey(call, UsersEndpoint(200, "All users that ever been on server"))
-            call.respondText(
-                JsonConverter.endpointJson(ep),
-                contentType = ContentType.Application.Json
-            )
+            withContext(Dispatchers.Default) {
+                val ep = Security.validateKey(call, UsersEndpoint(200, "All users that ever been on server"))
+                call.respondText(
+                    endpoint(ep),
+                    contentType = ContentType.Application.Json
+                )
+            }
         }
     }
 
     routing {
         get("/api/users/{username}") {
-            Security.validateKey(call, PlayerEndpoint(200, "Player information", call.parameters["username"] ?: "undefined"))
-            call.respondText(
-                """{"message": "The API successfully validated your access token."}""",
-                contentType = ContentType.Application.Json
-            )
+            withContext(Dispatchers.Default) {
+                val ep = Security.validateKey(
+                    call,
+                    PlayerEndpoint(200, "Player information", call.parameters["username"] ?: "undefined")
+                )
+                call.respondText(
+                    endpoint(ep),
+                    contentType = ContentType.Application.Json
+                )
+            }
         }
     }
+}
 
-    routing {
-
-    }
+suspend fun endpoint(ep: Endpoint) : String {
+    return JsonConverter.endpointJson(ep)
 }
